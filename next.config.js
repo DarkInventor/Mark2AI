9b50cbbd0d56bb699fa9116cdf28947ee647c5fa
DarkInventor <ktmehta25@gmail.com>
2023-09-18 22:00:04 +0530

    trying updated config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
-// module.exports = {
-//     webpack5: true,
-//     webpack: (config) => {
-//       config.resolve.fallback = { fs: false };
-  
-//       return config;
-//     },
-//   };
+// module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    // Add or customize Webpack configuration here
+    // For example, you can use 'alias' to replace specific modules
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      fs: false, // Disable 'fs' module
+      tls: false, // Disable 'tls' module
+      net: false, // Disable 'net' module
+    };
+    return config;
+  },
+};
